8dd43ee9a65ca51be3b2d3019c4455085755ea0d
darkpred1 <d.howells@live.com>
2019-09-29 14:28:36 +0100

    Module Completed

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,12 +10,12 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View("index");
+            return View("Index");
         }
 
         public IActionResult Error()
         {
-            return View("error");
+            return View("Error");
         }
     }
 }
